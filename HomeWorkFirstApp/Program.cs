963f960c6e3bf694a5cd7e7e5340d69251d471ec
bogovid <bogovid@gloria-jeans.ru>
2022-11-15 20:14:07 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWorkFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
